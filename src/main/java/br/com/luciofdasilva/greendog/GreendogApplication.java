d71b281d221b8d4e6355a1c9d40d4b2977114e9f
luciosilva <sandmanlucio@gmail.com>
2023-11-19 23:39:41 -0300

    start project

---
+++ src/main/java/br/com/luciofdasilva/greendog/GreendogApplication.java
@@ -0,0 +1,13 @@
+package br.com.luciofdasilva.greendog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreendogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreendogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/luciofdasilva/greendog/GreendogApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.luciofdasilva.greendog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreendogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
